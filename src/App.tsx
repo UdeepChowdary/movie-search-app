04223f17e7b20f7115463f32033a6a740c4b9cdc
UdeepChowdary <udeepchowdary@gmail.com>
2025-05-08 21:45:32 +0530

    Update BrowserRouter with correct basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,7 @@ const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <BrowserRouter>
+      <BrowserRouter basename="/movie-search-app">
         <Suspense
           fallback={
             <Box
